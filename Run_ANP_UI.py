328e883ba9dcdd3e16b1cb6ab246c7f8ffb69860
NATRIST-0 <tristgayrard@gmail.com>
2024-12-22 14:27:37 +0100

    added tooltips to headers

--- Run_ANP_UI.py
+++ Run_ANP_UI.py
@@ -1,6 +1,7 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from ANP import Ui_MainWindow
 import ANP_fonctions as af
+from PyQt6.QtCore import Qt
 
 class Window(QMainWindow):
     def __init__(self):
@@ -16,9 +17,18 @@ class Window(QMainWindow):
         self.ui_main_window.pushButton_import.clicked.connect(lambda: af.remplir_tableau(self.ui_main_window.table1, af.on_pushButton_import_clicked()))
 
 ##############################################################################################################
-
+    
         # Données et en-têtes de table1
-        headers_table1 = ["λ (Å)", "Raie", "I₀Gauss", "I₀/I(Hβ)=100", " I_c ", "Δ%"]
+        self.headers_table1 = ["λ (Å)", "Raie", "I₀Gauss", "I₀/I(Hβ)=100", " I_c ", "Δ%"]
+        self.ToolTips = [
+            "Longueur d'onde en Angströms",
+            "Nom de la raie spectrale",
+            "Intensité observée (Gauss)",
+            "Intensité relative à Hβ (normalisée à 100)",
+            "Intensité corrigée de l'extinction",
+            "Pourcentage de rebleuïssement"
+        ]
+
         data_table1 = [
             [4340.47, "H I", "", "", "", ""],
             [4363.21, "[O III]", "", "", "", ""],
@@ -35,10 +45,20 @@ class Window(QMainWindow):
             [6730.70, "[S II]", "", "", "", ""],
         ]
 
-        # Création et assignation du modèle
-        model1 = af.TableModel(data_table1, headers_table1)
-        self.ui_main_window.table1.setModel(model1)
+        # Création d'une classe modèle personnalisée avec des tooltips
+        class TableModelWithTooltips(af.TableModel):
+            def headerData(self, section, orientation, role=Qt.ItemDataRole.DisplayRole):
+                if orientation == Qt.Orientation.Horizontal:
+                    if role == Qt.ItemDataRole.DisplayRole:
+                        return self._headers[section]
+                    elif role == Qt.ItemDataRole.ToolTipRole:
+                        return window.ToolTips[section]
+                return None
 
+        # Création et assignation du modèle avec tooltips
+        model1 = TableModelWithTooltips(data_table1, self.headers_table1)
+        self.ui_main_window.table1.setModel(model1)
+        
         # Ajuster la taille des colonnes et des lignes automatiquement
         self.ui_main_window.table1.resizeColumnsToContents()
         self.ui_main_window.table1.resizeRowsToContents()
@@ -51,9 +71,9 @@ class Window(QMainWindow):
         # Données et en-têtes de table2
         headers_table2 = ["n", "Raie", "Valeur mesurée", "Valeur théorique", "ReBleuiement"]
         data_table2 = [
-            [3, "Hα", "", "", "", ""],
-            [4, "Hβ", "", "", "", ""],
-            [5, "Hγ", "", "", "", ""],
+            [3, "Hα", "", "", ""],
+            [4, "Hβ", "", "", ""],
+            [5, "Hγ", "", "", ""],
         ]
 
         # Création et assignation du modèle
@@ -66,8 +86,6 @@ class Window(QMainWindow):
 
         # Ajuster la taille du widget de la table en fonction du contenu
         af.adjustTableWidgetSize(self.ui_main_window.table2)
-
-##############################################################################################################
 
 if __name__ == "__main__":
     app = QApplication([])
